e8cf248d4e8282817e6a55674cfddcf1368e99fe
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-01-09 16:58:44 +0100

    Destroy block on click

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,34 +4,44 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    float time =0f, randX, randY;
-    [SerializeField] float _spawnRate,_distance,_movingSpeed;
+    float time = 0f, randX, randY;
+    [SerializeField] float _spawnRate, _distance, _movingSpeed;
     [SerializeField] GameObject _objectToSpawn;
     public List<GameObject> _spawnedObject;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime;
-        if(time>= 1/_spawnRate)
+        if (time >= 1 / _spawnRate)
         {
-            randX = Random.Range(0f,Camera.main.pixelWidth);
-            randY = Random.Range(0f,Camera.main.pixelHeight);
-            var camView = Camera.main.ScreenToWorldPoint(new Vector3(randX,randY,_distance));
-            var spawned = Instantiate(_objectToSpawn,camView,Quaternion.identity);
-            time =0f;
+            randX = Random.Range(0f, Camera.main.pixelWidth);
+            randY = Random.Range(0f, Camera.main.pixelHeight);
+            var camView = Camera.main.ScreenToWorldPoint(new Vector3(randX, randY, _distance));
+            var spawned = Instantiate(_objectToSpawn, camView, Quaternion.identity);
+            time = 0f;
             _spawnedObject.Add(spawned);
         }
-        foreach(GameObject item in _spawnedObject)
+        foreach (GameObject item in _spawnedObject)
         {
-            Debug.Log("hey");
-            item.transform.Translate(-Camera.main.transform.forward*Time.deltaTime*_movingSpeed);
+            item.transform.Translate(-Camera.main.transform.forward * Time.deltaTime * _movingSpeed);
         }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                _spawnedObject.Remove(hit.transform.gameObject);
+                Destroy(hit.transform.gameObject);
+            }
+        }
+
     }
 }
